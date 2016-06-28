bdb7b10d90f0d5fe8c63661519623ef559135b8e
Brig Young <Sonophoto@users.noreply.github.com>
2016-06-28 12:44:48 -0700

    Update slicingStrings.py

--- slicingStrings.py
+++ slicingStrings.py
@@ -1,5 +1,49 @@
+# *************************************************************************
+# How do I use slices to partition a string?
+# Use positional indexes of words to return sliced words:
+
 bd = "Blood Thirsty Dinosaur"
 b = bd[:5]
 t = bd[6:13]
 d = bd[-8:]
 print(b + "*" + t + "*" + d )
+
+
+del b; del t; del d
+# *************************************************************************
+# How do I find out if a substring is in a list? (True or False)
+# use the 'in' operator:
+
+L1 = "After this substring - Print Everything Else".split(" ")
+Boolean_answer = "substring" in L1
+
+
+# *************************************************************************
+# How do I return the location of a substring in a string?
+# Use the str.index(str) method:
+
+s1 = "After this substring - Print Everything Else"
+s2 = "substring"
+s1_idx_of_s2 = s1.index(s2)
+
+
+# *************************************************************************
+# How do I find out how long a string is?
+# use the len() function
+
+s1 = "After this substring - Print Everything Else"
+length_of_s1 = len(s1)
+
+
+# *************************************************************************
+# How do I return the rest of a string after some substring?
+# Use str.index(), add length of "substring", use this offset to slice it:
+
+s1 = "After this substring - Print Everything Else"
+s2 = "substring"
+s3 = s1[s1.index(s2) + len(s2):]
+
+
+
+
+
