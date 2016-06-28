ffa330a34a975d48dcfe2713b5f8a129d2a65373
Brig Young <brig@sonophotostudios.com>
2016-06-28 15:58:38 -0700

    Made small tweak to gcd_euclid.py and added gcd_lame.py

--- gcd_lame.py
+++ gcd_lame.py
@@ -1,12 +1,12 @@
-def gcd_euclid(a, b):
-    if(a == 0 or b == 0): return 0
+def gcd_lame(a, b):
+    if (a == 0 or b == 0): return 0
     while ( a != b):
         if (a > b):
             if (b == 0): return (a)
-            a = a -b
+            a = a % b
         else:
             if (a == 0): return (b)
-            b = b -a
+            b = b % a
     return a
 
 
@@ -14,21 +14,21 @@ def gcd_euclid(a, b):
 x = 1000
 y = 20
 z = 0
-z = gcd_euclid(x, y)
+z = gcd_lame(x, y)
 
 print(str(x) + " " +  str(y) + " " + str(z))
 
 x = 12121212
 y = 122
 z = 0
-z = gcd_euclid(x, y)
+z = gcd_lame(x, y)
 
 print(str(x) + " " +  str(y) + " " + str(z))
 
 x = 20
 y = 1000
 z = 0
-z = gcd_euclid(x, y)
+z = gcd_lame(x, y)
 
 print(str(x) + " " +  str(y) + " " + str(z))
 
@@ -36,7 +36,7 @@ print(str(x) + " " +  str(y) + " " + str(z))
 x = 99
 y = 11
 z = 0
-z = gcd_euclid(x, y)
+z = gcd_lame(x, y)
 
 print(str(x) + " " +  str(y) + " " + str(z))
 
@@ -46,63 +46,63 @@ print(str(x) + " " +  str(y) + " " + str(z))
 x = 0
 y = 0
 z = 0
-z = gcd_euclid(x, y)
+z = gcd_lame(x, y)
 
 print(str(x) + " " +  str(y) + " " + str(z))
 
 x = 0
 y = 1
 z = 0
-z = gcd_euclid(x, y)
+z = gcd_lame(x, y)
 
 print(str(x) + " " +  str(y) + " " + str(z))
 
 x = 1 
 y = 0
 z = 0
-z = gcd_euclid(x, y)
+z = gcd_lame(x, y)
 
 print(str(x) + " " +  str(y) + " " + str(z))
 
 x = 3 
 y = 2
 z = 0
-z = gcd_euclid(x, y)
+z = gcd_lame(x, y)
 
 print(str(x) + " " +  str(y) + " " + str(z))
 
 x = 2 
 y = 3
 z = 0
-z = gcd_euclid(x, y)
+z = gcd_lame(x, y)
 
 print(str(x) + " " +  str(y) + " " + str(z))
 
 x = 2 
 y = 1
 z = 0
-z = gcd_euclid(x, y)
+z = gcd_lame(x, y)
 
 print(str(x) + " " +  str(y) + " " + str(z))
 
 x = 1 
 y = 2
 z = 0
-z = gcd_euclid(x, y)
+z = gcd_lame(x, y)
 
 print(str(x) + " " +  str(y) + " " + str(z))
 
 x = 11
 y = 10
 z = 0
-z = gcd_euclid(x, y)
+z = gcd_lame(x, y)
 
 print(str(x) + " " +  str(y) + " " + str(z))
 
 x = 10 
 y = 11
 z = 0
-z = gcd_euclid(x, y)
+z = gcd_lame(x, y)
 
 print(str(x) + " " +  str(y) + " " + str(z))
 
