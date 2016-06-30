456f0d315cce87be82bc375323dfb5877387015c
Brig Young <brig@sonophotostudios.com>
2016-06-29 17:56:49 -0700

    Output tweaking so I don't look bad...lulz

--- sumston.py
+++ sumston.py
@@ -4,14 +4,16 @@ def sumston (A, B, N):
     for x in A:
         if (N - x) in B: output_triple(x, (N - x), N) 
             
+
 def output_triple(x, y, N):
     data_hash = {"x": str(x), "y": str(y), "N": str(N)}
     output_template = "{x} + {y} = {N}"
     output = output_template.format(**data_hash)
     print(output)
-   
 
+# ***  T E S T S  *****************************************************
 if __name__ == "__main__":
+    print("________________________________________________________________________")
     print("First pass with A, B, and X")
     A = [6, 5, 4]
     B = [1, 2, 3]
@@ -21,6 +23,7 @@ if __name__ == "__main__":
     print(X)
     sumston(A, B, X)
 
+    print("________________________________________________________________________")
     print("First pass with C, D, and Y")
     C = [0, 0, 0]
     D = [0, 0, 0]
@@ -30,7 +33,7 @@ if __name__ == "__main__":
     print(Y)
     sumston(C, D, Y)
 
-
+    print("________________________________________________________________________")
     print("First pass with E, F, and Z")
     F = [0, 6, 0]
     E = [3, 3, 3]
