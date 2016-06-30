2404c42a461323474a8e96168ee1d93241f03a0f
Brig Young <brig@sonophotostudios.com>
2016-06-29 17:52:27 -0700

    First pass at a solution to Dan's pet favorite interview question...

---
+++ sumston.py
@@ -0,0 +1,42 @@
+# Complexity on this should be O(n log n) with a tree worst case
+# This a List * Hash it could be O(2n) or rather O(n) best case
+def sumston (A, B, N):
+    for x in A:
+        if (N - x) in B: output_triple(x, (N - x), N) 
+            
+def output_triple(x, y, N):
+    data_hash = {"x": str(x), "y": str(y), "N": str(N)}
+    output_template = "{x} + {y} = {N}"
+    output = output_template.format(**data_hash)
+    print(output)
+   
+
+if __name__ == "__main__":
+    print("First pass with A, B, and X")
+    A = [6, 5, 4]
+    B = [1, 2, 3]
+    X = 7
+    print(" ".join(str(x) for x in A))
+    print(" ".join(str(x) for x in B))
+    print(X)
+    sumston(A, B, X)
+
+    print("First pass with C, D, and Y")
+    C = [0, 0, 0]
+    D = [0, 0, 0]
+    Y = 7
+    print(" ".join(str(x) for x in C))
+    print(" ".join(str(x) for x in D))
+    print(Y)
+    sumston(C, D, Y)
+
+
+    print("First pass with E, F, and Z")
+    F = [0, 6, 0]
+    E = [3, 3, 3]
+    Z = 9
+    print(" ".join(str(x) for x in E))
+    print(" ".join(str(x) for x in F))
+    print(Z)
+    sumston(E, F, Z)
+
