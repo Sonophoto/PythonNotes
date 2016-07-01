95aeab57f94e0f572355b095cda2ad68c4a785df
Brig Young <brig@sonophotostudios.com>
2016-06-30 21:17:49 -0700

    Next Iteration of Pythonizing Fibonacci

--- Fibonacci.py
+++ Fibonacci.py
@@ -1,27 +1,36 @@
-# Reference: https://oeis.org/search?q=fibonacci
-# F(n) = (F(n-1) + F(n-2) + ... + F(n-n)
-# F(0) = 0, F(1) == 1, F(2) == 1, F(3) == 3
-# 0, 1, 1, 3, 5, 8, 13, 21, 34, 55, 89, 144, 
+# Fibonacci Series: Pythonized sequencer
+# Implementation (c) 2016 Brig Young (github.com/Sonophoto)
+
 def Fibonacci(N):
-    idx, np, nn, fibnum = 1, 0, 1, 0
-    if not (N): return (fibnum)
+    idx, fibp, fib = 1, 0, 1
+    if not (N): return (fibp)
     while(True):
-        idx, np, nn, fibnum = ((idx + 1), nn, (np + nn), (np + nn)) 
-        if (idx >= N):
-            return (fibnum)
+        idx, fibp, fib = ((idx + 1), fib, (fibp + fib)) 
+        if (idx >= N): return (fib)
+
+
 
 if __name__ == "__main__":
+# Reference for this Series: https://oeis.org/search?q=fibonacci
+# OEIS: Online Encyclopedia of Integer Sequences
+# F(n) = (F(n-1) + F(n-2) with F(0) == 0 and F(1) == 1
+# F(0) = 0, F(1) == 1, F(2) == 1, F(3) == 2, F(4) == 3, ...
+# 0, 1, 1, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987
 
-    print(  0,  Fibonacci(0) )
-    print(  1,  Fibonacci(1) )
-    print(  2,  Fibonacci(2) )
-    print(  3,  Fibonacci(3) )
-    print(  4,  Fibonacci(4) )
-    print(  5,  Fibonacci(5) )
-    print(  6,  Fibonacci(6) )
-    print(  7,  Fibonacci(7) )
-    print(  8,  Fibonacci(8) )
-    print(  9,  Fibonacci(9) )
-    print( 10, Fibonacci(10) )
-    print( 11, Fibonacci(11) )
-    print( 12, Fibonacci(12) )
+    print(  0,  Fibonacci(0),    0)
+    print(  1,  Fibonacci(1),    1)
+    print(  2,  Fibonacci(2),    1)
+    print(  3,  Fibonacci(3),    2)
+    print(  4,  Fibonacci(4),    3)
+    print(  5,  Fibonacci(5),    5)
+    print(  6,  Fibonacci(6),    8)
+    print(  7,  Fibonacci(7),   13)
+    print(  8,  Fibonacci(8),   21)
+    print(  9,  Fibonacci(9),   34)
+    print( 10, Fibonacci(10),   55)
+    print( 11, Fibonacci(11),   89)
+    print( 12, Fibonacci(12),  144)
+    print( 13, Fibonacci(13),  233)
+    print( 14, Fibonacci(14),  377)
+    print( 15, Fibonacci(15),  610)
+    print( 16, Fibonacci(16),  987)
