1bd69920916bf7ed3ae908e70ef463ce565ff269
Brig Young <Sonophoto@users.noreply.github.com>
2016-07-06 16:47:27 -0700

    Machine readable documentation for several web APIs

---
+++ dataAPIs.py
@@ -0,0 +1,18 @@
+# This is a directory of Public Data APIs intended to be machine readable.
+#
+# We are using the US Governments "Project Open Data" format to encapsulate
+# the machine redable documentation for APIs
+# More info is here: https://project-open-data.cio.gov/v1.1/api/
+#
+"""
+"distribution": [
+    {
+        "accessURL": "http://www.agency.gov/api/vegetables/",
+        "description": "A fully queryable REST API with JSON and XML output",
+        "describedBy": "http://www.agency.gov/api/vegetables/swagger.json",
+        "describedByType": "application/swagger+json",
+        "format": "API",
+        "title": "Vegetables REST API"
+    }
+]
+"""
