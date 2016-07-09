985907a78da42043e7170f1c51ce1a45b42a3643
Brig Young <brig@sonophotostudios.com>
2016-07-09 11:45:00 -0700

    Editing Cleanup

--- Fibonacci.py
+++ Fibonacci.py
@@ -11,13 +11,12 @@ def Fibonacci(N):
     return (fib)
 
 
-
 if __name__ == "__main__":
-# Reference for this Series: https://oeis.org/search?q=fibonacci
-# OEIS: Online Encyclopedia of Integer Sequences
-# F(n) = (F(n-1) + F(n-2) with F(0) == 0 and F(1) == 1
-# F(0) = 0, F(1) == 1, F(2) == 1, F(3) == 2, F(4) == 3, ...
-# 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987
+    # Reference for this Series: https://oeis.org/search?q=fibonacci
+    # OEIS: Online Encyclopedia of Integer Sequences
+    # F(n) = (F(n-1) + F(n-2) with F(0) == 0 and F(1) == 1
+    # F(0) = 0, F(1) == 1, F(2) == 1, F(3) == 2, F(4) == 3, ...
+    # 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987
 
     fibs = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987]
 
