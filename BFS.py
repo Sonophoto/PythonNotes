88594554fb6a9ce94dfc92afe631f28db65ecc5f
Brig Young <Sonophoto@users.noreply.github.com>
2017-10-23 12:36:16 -0700

    Removing MIT code.

--- BFS.py
+++ BFS.py
@@ -1,26 +1,4 @@
-# This material was created or adapted from material created
-# by MIT faculty member Erik Demaine, Professor.
-# Copyright C 2011 Erik Demaine.
-# MIT License assumed, Modified 2016 Brig Young, github/Sonophoto
-parent = {}
-level = {}
- 
-def BFS(s, Adj):
-    level[s] = 0
-    parent[s] = None
-    i = 1
-    frontier = [s] # level i - 1
-    while frontier:
-        next = [] # level i
-        for u in frontier:
-            for v in Adj[u]: # edge from u to v
-                if v not in level:
-                    level[v] = i
-                    parent[v] = u
-                    next.append(v)
-                print("Current Vertex: ", v, "  Current Frontier: ", frontier, "  Adjacency@Vertex: ", Adj[v])
-        frontier = next
-        i += 1
+
 
 if __name__ == "__main__":
     vertices = ["s", "a", "c", "d", "f", "z", "x", "v"]
