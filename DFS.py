eb9113fef5ea361ef4f6ff5c5382f98991738990
Brig Young <Sonophoto@users.noreply.github.com>
2017-10-23 12:36:49 -0700

    Removing MIT code

--- DFS.py
+++ DFS.py
@@ -1,22 +1,3 @@
-# This material was created or adapted from material created
-# by MIT faculty member Erik Demaine, Professor.
-# Copyright C 2011 Erik Demaine.
-# MIT License assumed, Modified 2016 Brig Young, github/Sonophoto
-
-parent = {}
-
-def DFS_Visit(Adj, s): 
-    for v in Adj[s]:
-        if v not in parent:
-            parent[v] = s
-            DFS_Visit(Adj, v)
-              
-def DFS(V, Adj):
-    for s in V:
-        if s not in parent:
-            parent[s] = None
-            DFS_Visit(Adj, s)
-        print("Vertex: ", s, "  Adjacency@Vertex: ", Adj[s])
 
 if __name__ == "__main__":
     vertices = ["a", "b", "c", "d", "e", "f"]
