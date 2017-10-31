fd65e755dab7a6a5df13cf9d527dc9b97d8c00f5
Brig Young <brig@sonophotostudios.com>
2017-10-31 13:34:36 -0700

    Adding additional background info on gcd

--- gcd_euclid.py
+++ gcd_euclid.py
@@ -1,3 +1,8 @@
+# The purpose of this file is to demonstrate the actual algorythm of 
+# Euclid in such a manner that Gabriel Lame's Modification is a trivial
+# modification of the function. And to point out that Euclid IN FACT
+# Used subtraction not %.
+
 def gcd_euclid(a, b):
     if(a == 0 or b == 0): return 0
     while ( a != b):
@@ -9,9 +14,22 @@ def gcd_euclid(a, b):
             b = b - a
     return a
 
-
-
-
+# RECURSION: Code by github/endolith that demonstrates a wrapper that
+# does a recursive reduction on an input list:
+# https://gist.github.com/endolith/114336
+#
+# PYTHON LIBRARY: fractions.gcd()
+# Return the greatest common divisor of the integers a and b.
+# If either a or b is nonzero, then the absolute value of gcd(a, b)
+# is the largest integer that divides both a and b. gcd(a,b) has the
+# same sign as b if b is nonzero; otherwise it takes the sign of a.
+# gcd(0, 0) returns 0.
+#
+# Concrete Math, Graham, Knuth, Patashnik, pp. 103:
+# says gcd(0,0) is undefined, i.e. gcd(0,0):return NaN
+# 
+# 
+#
 # Some easy cases:
 x = 1000
 y = 20
