83d3d1b9556800eab912ae7082abb6a5185c4c90
Brig Young <brig@sonophotostudios.com>
2017-11-08 17:21:34 -0800

    Wrapped questions in pythonic comment delimiters

--- slicingStrings.py
+++ slicingStrings.py
@@ -2,8 +2,10 @@
 # Don't let anyone fool you, 2.7 and 3.4 are different languages...
 
 # *************************************************************************
-# How do I use slices to partition a string?
-# Use positional indexes of substrings to return sliced strings:
+"""
+ How do I use slices to partition a string?
+ Use positional indexes of substrings to return sliced strings:
+"""
 bd = "Blood Thirsty Dinosaur"
 
 # Make a list of words by offsets:
@@ -33,10 +35,13 @@ if (new_bd == bd): print("Hooray!")
 
 
 del L1, bd, new_bd
+
 # *************************************************************************
-# How do I print out a list of strings?
-# You can print it directly, element by element, or join() it into a str.
-# You could even use a completion
+"""
+ How do I print out a list of strings?
+ You can print it directly, element by element, or join() it into a str.
+ You could even use a completion
+"""
 bd = "Blood Thirsty Dinosaur"
 L1 = [bd[:5], bd[6:13], bd[-8:]]
 
@@ -52,67 +57,81 @@ DevNull = [print(x, end=' ') for x in L1]
 
 
 del L1, bd, x, DevNull
-# *************************************************************************
-# How do I find out if a substring is in a list? (True or False)
-# use the 'in' operator:
 
+# *************************************************************************
+"""
+ How do I find out if a substring is in a list? (True or False)
+ use the 'in' operator:
+"""
 L1 = "After this substring - Print Everything Else".split(" ")
 Boolean_answer = "substring" in L1
 
 
 del L1, Boolean_answer
-# *************************************************************************
-# How do I return the location of a substring in a string?
-# Use the str.index(str) method:
 
+# *************************************************************************
+"""
+ How do I return the location of a substring in a string?
+ Use the str.index(str) method:
+"""
 s1 = "After this substring - Print Everything Else"
 s2 = "substring"
 s1_idx_of_s2 = s1.index(s2)
 
 
 del s1, s2, s1_idx_of_s2
-# *************************************************************************
-# How do I find out how long a string is?
-# use the len() function
 
+# *************************************************************************
+"""
+ How do I find out how long a string is?
+ use the len() function
+"""
 s1 = "After this substring - Print Everything Else"
 length_of_s1 = len(s1)
 
 
 del s1, length_of_s1
-# *************************************************************************
-# How do I return the rest of a string after some substring?
-# Use str.index(), add length of "substring", use this offset to slice it:
 
+# *************************************************************************
+"""
+ How do I return the rest of a string after some substring?
+ Use str.index(), add length of "substring", use this offset to slice it:
+"""
 s1 = "After this substring - Print Everything Else"
 s2 = "substring"
 s3 = s1[s1.index(s2) + len(s2):]
 
 
 del s1, s2, s3
-# *************************************************************************
-# How do I convert a list of ints into a space delimited string of numbers?
-# compose a call to " ".join() with a list completion:
 
+# *************************************************************************
+"""
+ How do I convert a list of ints into a space delimited string of numbers?
+ compose a call to " ".join() with a list completion:
+"""
 L1 = [1, 22, 13, 42, 451, 66, 7, 8, 1999]
 s1 = " ".join(str(x) for x in L1)
 
 
 del L1, s1
-# *************************************************************************
-# How do I convert a space delimeted string of numbers into a list of ints?
-# Use a list completion to generate a list from str.split()
-# (It is usually better to use regexes on real data)
 
+# *************************************************************************
+"""
+ How do I convert a space delimeted string of numbers into a list of ints?
+ Use a list completion to generate a list from str.split()
+ (It is usually better to use regexes on real data)
+"""
 s1 = "1 22 13 42 451 66 7 8 1999"
 L1 = [int(x) for x in s1.split(" ")]
 
 
 del L1, s1
-# *************************************************************************
-# How do I parameterize a string with a dictionary of values?
-# Use the .format(**kwargs) method:
 
+# *************************************************************************
+"""
+ How do I parameterize a string with a dictionary of values?
+ Use the .format(**kwargs) method:
+"""
 D1 = {"temp": str(28), "humid": str(77), "pres": str(1066)}
 s1 = "Local Weather: Temperature: {temp}, Humidity: {humid}, Pressure: {pres}"
 s1.format(**D1)
