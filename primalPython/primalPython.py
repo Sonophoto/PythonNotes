f93d9a8fa732c729b9c002cdb86e6a6d36e75264
Brig Young <Sonophoto@users.noreply.github.com>
2020-12-31 19:19:23 -0800

    Added classic prime finder function from cpython docs

--- primalPython/primalPython.py
+++ primalPython/primalPython.py
@@ -40,6 +40,18 @@ def digitalSum(number):
     for c in number: digital_sum += int(c)
     return digital_sum
 
+def pyPrimeFinder(number):
+   """This is taken directly from python docs, thrown in for fun, this is a classic method
+      https://docs.python.org/3/tutorial/controlflow.html#break-and-continue-statements-and-else-clauses-on-loops
+   """   
+   for n in range(2, 10):
+      for x in range(2, n):
+         if n % x == 0:
+            print(n, 'equals', x, '*', n//x)
+            break
+      else:
+      # loop fell through without finding a factor
+      print(n, 'is a prime number')
 
 def _validate(number):
     """ Verify that returned value is valid string representation of an
